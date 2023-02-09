fe0045ca7df1a30ff46fa76b3b665ba1af10ddd8
a805431 <gergana.ivanova@yara.com>
2023-02-09 18:02:42 +0200

    json Samples included

--- assets/scripts/converter.js
+++ assets/scripts/converter.js
@@ -7,6 +7,14 @@ const convertBtn = document.getElementById('btn-convert-json-to-csv');
 const clearBtn = document.getElementById('btn-clear-json-and-csv');
 const formatBtn = document.getElementById('btn-format-json-text');
 
+function validateJsonFormat(jsonContent){
+    if(jsonContent){
+
+    }
+}
+
+//това е пробно засега
+//пробвай нещо с map()
 function fillCsvBoxWithJsonBoxContents(){
     const jsonBoxContent = jsonInput.value;
     csvOutput.value = jsonBoxContent;
