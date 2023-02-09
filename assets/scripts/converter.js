43bb648b66cb852ad0b25d6c0d0f412418e32899
a805431 <gergana.ivanova@yara.com>
2023-02-09 17:22:15 +0200

    partial button functionality added

--- assets/scripts/converter.js
+++ assets/scripts/converter.js
@@ -7,5 +7,16 @@ const convertBtn = document.getElementById('btn-convert-json-to-csv');
 const clearBtn = document.getElementById('btn-clear-json-and-csv');
 const formatBtn = document.getElementById('btn-format-json-text');
 
+function fillCsvBoxWithJsonBoxContents(){
+    const jsonBoxContent = jsonInput.value;
+    csvOutput.value = jsonBoxContent;
+}
 
+function clearJsonAndCsvBoxContents(){
+    jsonInput.value = "";
+    csvOutput.value = "";
+}
+
+convertBtn.addEventListener('click', fillCsvBoxWithJsonBoxContents);
+clearBtn.addEventListener('click', clearJsonAndCsvBoxContents);
 
