a424d3ae769d07b71f51414be7a8315e9c68696e
a805431 <gergana.ivanova@yara.com>
2023-02-09 16:59:55 +0200

    format and clear buttons added

---
+++ assets/scripts/converter.js
@@ -0,0 +1,11 @@
+//Text areas
+const jsonInput = document.getElementById('json-format-input');
+const csvOutput = document.getElementById('csv-format-output');
+
+//Buttons
+const convertBtn = document.getElementById('btn-convert-json-to-csv');
+const clearBtn = document.getElementById('btn-clear-json-and-csv');
+const formatBtn = document.getElementById('btn-format-json-text');
+
+
+
