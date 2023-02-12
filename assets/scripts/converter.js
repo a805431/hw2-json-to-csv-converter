d0a4891a9476cb8c8049055f1f1244394cdf37c3
a805431 <gergana.ivanova@yara.com>
2023-02-12 16:53:09 +0200

    new comment

--- assets/scripts/converter.js
+++ assets/scripts/converter.js
@@ -10,7 +10,8 @@ const formatBtn = document.getElementById('btn-format-json-text');
 //patterns
 //base pattern
 const baseJsonPattern = /\"\w*\"\:\s?((\"\w*\")|\d+|false|true|null)/g;
-const objectJsonPattern = /\{\s*\"\w*\"\:\s?((\"\w*\")|\d+|false|true|null)\s*\}/g;
+const objectJsonPattern =
+  /\{\s*\"\w*\"\:\s?((\"\w*\")|\d+|false|true|null)\s*\}/g;
 const curlyBracePattern = /\{[^}]+\}/gm;
 const squareBracketsPattern = /\[[^]+\]/gm;
 
@@ -19,35 +20,54 @@ const squareBracketsPattern = /\[[^]+\]/gm;
 //     csvOutput.value = jsonBoxContent;
 // }
 
-function validateJsonFormat(){
-    const jsonBoxContent = jsonInput.value;
-    let outerSquaresMatch = jsonBoxContent.match(squareBracketsPattern);
-    let objectPatternMatches = []; //масив от елементи със синтаксис на JSON обекти
-
-    if(jsonBoxContent != "" && outerSquaresMatch != null){
-        alert("VALID INPUT!" + " " + outerSquaresMatch[0]);
-
-        for(let elem of outerSquaresMatch){
-            //първо на елементът трябва да му се премахнат квадратните скоби
-            const elemWithoutSquareBrackets = elem.slice(1, -1);
-
-            objectPatternMatches = elemWithoutSquareBrackets.match(curlyBracePattern);
-
-            if(objectPatternMatches != null){
-                alert("PUSHED ELEM IS: "+objectPatternMatches[0]);
-            }
-        }
-    }else{
-        alert("INVALID INPUT");
-    }
+function removeBrackets(singleElement) {
+  const result = singleElement.slice(1, -1);
+  return result;
 }
 
-function clearJsonAndCsvBoxContents(){
-    jsonInput.value = "";
-    csvOutput.value = "";
+function doesMatchBaseJsonPattern(stringToBeMatched) {
+  const booleanResult = stringToBeMatched.match(baseJsonPattern) != null;
+  return booleanResult;
+}
+
+function validateJsonFormat() {
+  const jsonBoxContent = jsonInput.value;
+  let outerSquaresMatch = jsonBoxContent.match(squareBracketsPattern);
+  let objectPatternMatches = []; //масив от елементи със синтаксис на JSON обекти
+  let separatedJSNObjectsAsStrings = [];
+
+  if (jsonBoxContent != '' && outerSquaresMatch != null) {
+    alert('VALID INPUT!' + ' ' + outerSquaresMatch[0]);
+
+    for (let elem of outerSquaresMatch) {
+      //първо на елементът трябва да му се премахнат квадратните скоби
+      const elemWithoutSquareBrackets = elem.slice(1, -1);
+
+      //съдържанието на квадратните скоби; много елементи, заобиколени от къдрави скоби
+      objectPatternMatches = elemWithoutSquareBrackets.match(curlyBracePattern);
+    }
+
+    if (objectPatternMatches != null) {
+      alert('PUSHED ELEM IS: ' + objectPatternMatches[1]);
+
+      for (let elem of objectPatternMatches) {
+        const separatedJSNObject = removeBrackets(elem); //elem е един JSON обект с {}
+
+       // array от стрингове с всеки елемент стринг, съдържащ двойки ключ стойност на един JSON обект
+        separatedJSNObjectsAsStrings.push(separatedJSNObject);
+        console.log(separatedJSNObject);
+      }
+    }
+  } else {
+    alert('INVALID INPUT');
+  }
+}
+
+function clearJsonAndCsvBoxContents() {
+  jsonInput.value = '';
+  csvOutput.value = '';
 }
 
 convertBtn.addEventListener('click', validateJsonFormat);
 clearBtn.addEventListener('click', clearJsonAndCsvBoxContents);
 //formatBtn.addEventListener('click', fillCsvBoxWithJsonBoxContents);
-
